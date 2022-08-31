55dc5412945f24e9a5086fa79c23ace3a5a4c839
Qwerci <theowerci@gmail.com>
2022-08-31 15:04:10 +0000

    changes region for frontend smoke test

--- frontend.sh
+++ frontend.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 function a(){
-    URL="http://udapeople-${CIRCLE_WORKFLOW_ID:0:7}.s3-website-us-east-1.amazonaws.com/#/employees"
+    URL="http://udapeople-${CIRCLE_WORKFLOW_ID:0:7}.s3-website-eu-west-1.amazonaws.com/#/employees"
     echo ${URL}
     if curl -s ${URL} | grep "Welcome"; then
         #Change this to 0 after the job fails
